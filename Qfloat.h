189fdcdf0b193f9fc2bb1f8e96d2860463603209
duclam227 <55783513+duclam227@users.noreply.github.com>
2020-04-27 21:33:23 +0700

    Update a bunch of things :v

--- Qfloat.h
+++ Qfloat.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
+#include <string>
 
+#define KNUMBER 16383	// số thừa K
 using namespace std;
 
 class Qfloat
@@ -10,9 +12,30 @@ private:
 public:
 	Qfloat();
 	~Qfloat();
+	bool GetBit(int);
+	void SetBit(int, bool);
 };
 
 void ScanQfloat(Qfloat&);
 void PrintQfloat(Qfloat);
 Qfloat BinToDec(bool* bit);
 bool* DecToBin(Qfloat x);
+
+bool CheckMinus(char first);
+bool CheckNumber(char input);
+bool CheckOne(string input);
+bool CheckDecimalPoint(char input);
+void NormalizeNumber(string& number);
+string DivideByTwo(string number);
+string DivideOne(string number);
+string IntegerDecToBin(string number);
+string FractionalDecToBin(string number);
+string FractionalBinToDec(string bin);
+string IntegerBinToDec(string bin);
+string MultiplyByTwo(string number);
+string MultiplyByFive(string number);
+void PowOfTwo(string pow[128]);
+void PowOfFive(string pow[128]);
+void PowOfTwoMinus(string pow[128]);
+string SumNumbers(string n1, string n2);
+string SumFractionals(string n1, string n2);
