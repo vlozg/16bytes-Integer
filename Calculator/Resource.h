5ceed75e3e25684cade21de50d44d653abea0c90
LongVu <55818681+vulong61@users.noreply.github.com>
2020-05-02 17:44:31 +0700

    Initiate UI (dialog)

--- Calculator/Resource.h
+++ Calculator/Resource.h
@@ -2,17 +2,55 @@
 // Microsoft Visual C++ generated include file.
 // Used by Calculator.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_CALCULATOR_DIALOG				102
+#define IDD_CALCULATOR_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define BtnNumA                         1000
+#define BtnChangeSign                   1001
+#define BtnNum0                         1002
+#define BtnDot                          1003
+#define IDC_EDIT1                       1004
+#define BtnNumB                         1005
+#define BtnNum7                         1006
+#define BtnNum8                         1007
+#define BtnNum9                         1008
+#define BtnNumC                         1009
+#define BtnNum4                         1010
+#define BtnNum5                         1011
+#define BtnNum6                         1012
+#define BtnEqual                        1013
+#define BtnPlus                         1014
+#define BtnMinus                        1015
+#define BtnNumD                         1016
+#define BtnNum1                         1017
+#define BtnNum2                         1018
+#define BtnNum3                         1019
+#define BtnMultiply                     1020
+#define BtnDivision                     1021
+#define BtnNumE                         1022
+#define BtnAnd                          1023
+#define BtnOr                           1024
+#define BtnXor                          1025
+#define BtnNot                          1026
+#define IDC_BUTTON27                    1027
+#define BtnBackspace                    1027
+#define BtnNumF                         1028
+#define BtnRightSihft                   1029
+#define BtnLeftShift                    1030
+#define BtnRolRight                     1031
+#define BtnRolLeft                      1032
+#define BtnClear                        1033
+#define BtnBin                          1034
+#define BtnHex                          1035
+#define IDC_EDIT2                       1036
+#define IDC_EDIT3                       1037
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
