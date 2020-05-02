b1bcfa192b3c6a6c083cfffd891214ba741a6a41
duclam227 <55783513+duclam227@users.noreply.github.com>
2020-05-02 16:24:33 +0700

    Thêm 1 số hàm

--- Qfloat.h
+++ Qfloat.h
@@ -1,4 +1,5 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 
@@ -9,17 +10,23 @@ class Qfloat
 {
 private:
 	char value[16];
+	int length;
 public:
 	Qfloat();
 	~Qfloat();
 	bool GetBit(int);
 	void SetBit(int, bool);
+	int GetLength();
+	void SetLength(int);
 };
 
+bool ReadDecString(string, Qfloat&);
 void ScanQfloat(Qfloat&);
 void PrintQfloat(Qfloat);
 Qfloat BinToDec(bool* bit);
 bool* DecToBin(Qfloat x);
+
+void ReadFile();
 
 bool CheckMinus(char first);
 bool CheckNumber(char input);
@@ -39,6 +46,7 @@ void PowOfFive(string pow[128]);
 void PowOfTwoMinus(string pow[128]);
 string SumNumbers(string n1, string n2);
 string SumFractionals(string n1, string n2);
+void DeleteExcessiveZero(string&);
 void PrintBinary(Qfloat x);
 bool CheckAllChar(string, char);
-void RoundUp(string);
+bool* StringToBool(string);
