8fddc014dd0155d6d3ad662cb17491a7e6e2288f
LongVu <55818681+vulong61@users.noreply.github.com>
2020-05-06 16:19:22 +0700

    Update change mode
    Thêm disable nút dot (để dành enable sau cho float mode)

--- Calculator/CalculatorDlg.cpp
+++ Calculator/CalculatorDlg.cpp
@@ -216,13 +216,14 @@ void CCalculatorDlg::UpdateAllData()
 //Thiết lập input thập phân
 void CCalculatorDlg::ChangeMode_Dec() 
 {
-	//Disable các nút input giá trị từ A - F
+	//Disable các nút input giá trị từ A - F và dot
 	b_NumA.EnableWindow(0);
 	b_NumB.EnableWindow(0);
 	b_NumC.EnableWindow(0);
 	b_NumD.EnableWindow(0);
 	b_NumE.EnableWindow(0);
 	b_NumF.EnableWindow(0);
+	b_Dot.EnableWindow(0);
 
 	//Enable các nút input từ 2 - 9
 	b_Num2.EnableWindow(1);
@@ -267,6 +268,9 @@ void CCalculatorDlg::ChangeMode_Dec()
 //Thiết lập input thập lục phân
 void CCalculatorDlg::ChangeMode_Hex() 
 {
+	//Disable nút dot
+	b_Dot.EnableWindow(0);
+
 	//Enable tất cả các nút input
 	b_NumA.EnableWindow(1);
 	b_NumB.EnableWindow(1);
@@ -331,7 +335,8 @@ void CCalculatorDlg::ChangeMode_Bin()
 	b_Num7.EnableWindow(0);
 	b_Num8.EnableWindow(0);
 	b_Num9.EnableWindow(0);
-	
+	b_Dot.EnableWindow(0);
+
 	//Swap active input với bin input
 	CString temp;
 	if (mode == 16)
